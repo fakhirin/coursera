51c16489cc71886f9c8fd270ac8f3d82f51bc0da
fakhirin <fakhirin.fauzi@hdcglobal.com>
2017-02-27 11:27:35 +0800

    GettingData

--- function.r
+++ function.r
@@ -12,11 +12,11 @@ above <- function(x, n = 10){
   x[use]
 }
 
-columnmean <- function(y){
+columnmean <- function(y, removeNA = TRUE){
   nc <- ncol(y)
   means <- numeric(nc)
   for(i in 1:nc){
-    means[i] <- means(y[, i], na.rm = removeNA)
+    means[i] <- mean(y[, i], na.rm = removeNA)
   }
   means
 }
